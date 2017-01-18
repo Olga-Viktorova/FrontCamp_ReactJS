2f20ccda86469d50be824158aafc5e3a0950e690
Volha Viktarava <Volha_Viktarava@epam.com>
2017-01-18 14:43:07 +0300

    MongooseConf

--- mongooseConf.js
+++ mongooseConf.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://volha:123456@ds041506.mlab.com:41506/heroku_5mldxthd');
 
 module.exports = mongoose;
  
